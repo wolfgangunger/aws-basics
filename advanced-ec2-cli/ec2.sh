999b0adafc3bbf1663c0c25bd44ef5f021f17019
Wolfgang Unger <wolfgang.unger@nttdata.com>
2018-07-02 18:58:12 +0200

    ec2-cf

--- advanced-ec2-cli/ec2.sh
+++ advanced-ec2-cli/ec2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-aws ec2 describe-instances --region eu-central-1
 aws ec2 describe-instances --region sa-east-1
 aws ec2 describe-instances --region sa-east-1 --query Reservations[].Instances[].InstanceId
 aws ec2 describe-instances --region sa-east-1 --filters "Name=instance-type,Values=t2.micro" --query Reservations[].Instances[].InstanceId
@@ -8,11 +7,11 @@ aws ec2 run-instances --image-id ami-6dca9001 --security-group-ids sg-7ee81907 -
 
 aws ec2 run-instances --image-id ami-6dca9001 --subnet-id subnet-9f0284f8 --security-group-ids sg-7ee81907 --count 1 --instance-type t2.micro --key-name ungerw-sp
 
-aws ec2 create-tags --resources i-0102ae2a8c41715e8 --tags Key=Name,Value=MyWebserver
+aws ec2 create-tags --resources i-0b4a0702930e42aed --tags Key=Name,Value=MyWebserver
 
-aws ec2 stop-instances --instance-ids i-0102ae2a8c41715e8
+aws ec2 stop-instances --instance-ids i-0b4a0702930e42aed
 
-aws ec2 start-instances --instance-ids i-0102ae2a8c41715e8
+aws ec2 start-instances --instance-ids i-0b4a0702930e42aed
 
 aws ec2 terminate-instances --instance-ids i-0102ae2a8c41715e8
 
