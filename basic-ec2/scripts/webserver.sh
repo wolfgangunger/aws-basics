a92d2f453afebb6cccd35857e24954f8a2aaddd6
ungerw <wolfgang.unger@nttdata.com>
2018-10-16 14:49:07 +0200

    changes

--- basic-ec2/scripts/webserver.sh
+++ basic-ec2/scripts/webserver.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-yum install httpd -y
-yum update -y
-aws s3 cp s3://YOURBUCKETNAMEHERE/index.html /var/www/html/
-service httpd start
-chkconfig httpd on
+sudo yum install httpd -y
+sudo yum update -y
+#start index
+#copy index file
+cd /var/www/html/
+#aws s3 cp s3://YOURBUCKETNAMEHERE/index.html /var/www/html/
+sudo wget https://s3.eu-central-1.amazonaws.com/ungerw-aws-examples/index.html
+# create it
+#cd /var/www/html/
+#sudo nano index.html 
+# end index 
+sudo service httpd start
+sudo chkconfig httpd on
